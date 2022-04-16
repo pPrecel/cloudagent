7a3a944b843b57db11f671d0c810dfefd5f83335
pPrecel <filip.strozik@outlook.com>
2022-04-16 13:21:33 +0200

    safe LastState for operations in many threads

--- internal/agent/state.go
+++ internal/agent/state.go
@@ -1,6 +1,8 @@
 package agent
 
 import (
+	"sync"
+
 	"github.com/gardener/gardener/pkg/apis/core/v1beta1"
 )
 
@@ -17,12 +19,18 @@ type StateSetter interface {
 
 type LastState struct {
 	v *v1beta1.ShootList
+	m sync.Mutex
 }
 
 func (s *LastState) Set(shoots *v1beta1.ShootList) {
+	s.m.Lock()
 	s.v = shoots
+	s.m.Unlock()
 }
 
 func (s *LastState) Get() *v1beta1.ShootList {
+	s.m.Lock()
+	defer s.m.Unlock()
+
 	return s.v
 }
