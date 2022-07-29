098fe4e8d45f6de9e82be361392462a273c01a18
pPrecel <filip.strozik@outlook.com>
2022-07-29 23:21:02 +0200

    fix

--- hack/build.sh
+++ hack/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-GIT_REF="$(git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null)"
-STATUS="$?"
-
-if [ "$STATUS" != "0" ]; then
-    export GIT_REF="$(git branch --show-current)"
+export GIT_REF="local"
+if [ "$GITHUB_REF_NAME" != "" ]; then
+    GIT_REF="$GITHUB_REF_NAME"
 fi
 
+echo "building cloudagent with version: $GIT_REF"
 go build -ldflags="-X 'main.Version=${GIT_REF}'" -o .out/cloudagent main.go
